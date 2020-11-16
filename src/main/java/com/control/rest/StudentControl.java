706b4265c713dec6e9ee99c7036a99204ca7428a
Admin <mihailbichkov@yandex.ru>
2020-11-16 20:41:31 +0500

    global exception handling

--- src/main/java/com/control/rest/StudentControl.java
+++ src/main/java/com/control/rest/StudentControl.java
@@ -1,12 +1,9 @@
 package com.control.rest;
 
-import com.ex.StudentErrorResponse;
 import com.ex.StudentNotFoundException;
 import com.model.Student;
 import com.service.StudentSvc;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,29 +27,5 @@ public class StudentControl {
         } catch (Exception e) {
             throw new StudentNotFoundException("Not found student with id " + id);
         }
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
